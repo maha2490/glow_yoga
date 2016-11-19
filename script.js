40a38fada12fcaeae94861ed6ea377e762361f76
Rachel Mahan <rachelm2490@gmail.com>
2016-11-19 14:45:06 -0500

    added google map

--- script.js
+++ script.js
@@ -2,5 +2,18 @@ $(document).ready(function(){
 
 
 
-
 });
+	
+
+
+function initMap() {
+	var uluru = {lat: 25.789, lng: -80.140};
+	var map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 18,
+		center: uluru
+	});
+	var marker = new google.maps.Marker({
+		position: uluru,
+		map: map
+	});
+}
