921b5aec485a63868d1624b2dec1903e747d0b9b
Rachel Mahan <rachelm2490@gmail.com>
2016-11-19 23:49:14 -0500

    smaller contact page layout changes

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ $(document).ready(function(){
 function initMap() {
 	var uluru = {lat: 25.789, lng: -80.140};
 	var map = new google.maps.Map(document.getElementById('map'), {
-		zoom: 18,
+		zoom: 16,
 		center: uluru
 	});
 	var marker = new google.maps.Marker({
