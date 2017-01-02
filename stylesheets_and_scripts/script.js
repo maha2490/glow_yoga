e12d50e691f12a852596de095cf5dfadd658c280
Rachel Mahan <rachelm2490@gmail.com>
2017-01-02 14:41:34 -0500

    new glow marker

--- stylesheets_and_scripts/script.js
+++ stylesheets_and_scripts/script.js
@@ -22,15 +22,15 @@ $(document).ready(function(){
 var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 16,
+    zoom: 17,
     center: new google.maps.LatLng(25.789, -80.140),
     mapTypeId: 'roadmap'
   });
 
   // ICONS_____________
   var glowImage = {
-      url: 'assets/glow_marker3.png', // image is 512 x 512
-      scaledSize : new google.maps.Size(52, 52),
+      url: 'assets/glow_marker6.png', // image is 512 x 512
+      scaledSize : new google.maps.Size(62, 62),
   };
   var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
   var icons = {
@@ -60,7 +60,10 @@ function initMap() {
       position: new google.maps.LatLng(25.7869427, -80.138672),
       type: 'parking'
     }, {
-      position: new google.maps.LatLng(25.7885976, -80.1402493),
+      // changing the marker positioning:
+      // move at the thousandths, increase lat moves up on x axis
+      // and                      increase lng moves left on y axis                            
+      position: new google.maps.LatLng(25.7887976, -80.1403493), 
       type: 'glowYoga'
       }
   ];
@@ -68,7 +71,7 @@ function initMap() {
   for (var i = 0, feature; feature = features[i]; i++) {
     addMarker(feature);
   }
-
+  // legend, if you want to reactivate. will need glow marker img (couldn't figure out initially)
   // var legend = document.getElementById('legend');
   // for (var key in icons) {
   //   var type = icons[key];
@@ -82,121 +85,3 @@ function initMap() {
   // map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(legend);
 }
 
-// var map;
-//   function initMap() {
-
-//   	var glow_yoga = {lat: 25.789, lng: -80.140};
-
-//   	var marker = new google.maps.Marker({
-//   		position: glow_yoga,
-//   		map: map
-//   	})
-//     map = new google.maps.Map(document.getElementById('map'), {
-//       zoom: 16,
-//       center: glow_yoga,
-//       mapTypeId: 'roadmap'
-//     });
-
-//     var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
-//     var icons = {
-//       parking: {
-//         name: 'Parking',
-//         icon: iconBase + 'parking_lot_maps.png'
-//       }
-//     };
-
-//     function addMarker(feature) {
-//       var marker = new google.maps.Marker({
-//         position: feature.position,
-//         icon: icons[feature.type].icon,
-//         map: map
-//       });
-//     }
-
-//     var features = [
-//       {
-//         position: new google.maps.LatLng(25.7897423, -80.1400118),
-//         type: 'parking'
-//       }, {
-//         position: new google.maps.LatLng(25.7869427, -80.138672),
-//         type: 'parking'
-//       }, {
-//         position: new google.maps.LatLng(25.7869427, -80.138672),
-//         type: 'parking'
-//       }
-//     ];
-
-//     for (var i = 0, feature; feature = features[i]; i++) {
-//       addMarker(feature);
-//     }
-
-    // var legend = document.getElementById('legend');
-    // for (var key in icons) {
-    //   var type = icons[key];
-    //   var name = type.name;
-    //   var icon = type.icon;
-    //   var div = document.createElement('div');
-    //   div.innerHTML = '<img src="' + icon + '"> ' + name;
-    //   legend.appendChild(div);
-    // }
-
-    // map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(legend);
-  // }
-
-
-// TRY 2 GOOGLE MAP THINGS _________________________
-
-// /*
-//  * declare map as a global variable
-//  */
-// var map;
-
-// /*
-//  * use google maps api built-in mechanism to attach dom events
-//  */
-// google.maps.event.addDomListener(window, "load", function () {
-
-//   /*
-//    * create map
-//    */
-//   var map = new google.maps.Map(document.getElementById("map"), {
-//     center: new google.maps.LatLng(25.789, -80.140),
-//     zoom: 16,
-//     mapTypeId: google.maps.MapTypeId.ROADMAP
-//   });
-
-//   /*
-//    * create infowindow (which will be used by markers)
-//    */
-//   var infoWindow = new google.maps.InfoWindow();
-
-//   /*
-//    * marker creater function (acts as a closure for html parameter)
-//    */
-//   function createMarker(options, html) {
-//     var marker = new google.maps.Marker(options);
-//     if (html) {
-//       google.maps.event.addListener(marker, "click", function () {
-//         infoWindow.setContent(html);
-//         infoWindow.open(options.map, this);
-//       });
-//     }
-//     return marker;
-//   }
-
-//   /*
-//    * add markers to map
-//    */
-    
-//     var image = {
-//         url: 'assets/glow_favicon.png', // image is 512 x 512
-//         scaledSize : new google.maps.Size(42, 32),
-//     };
-    
-//   var marker0 = createMarker({
-//     position: new google.maps.LatLng(25.789, -80.140),
-//     map: map,
-//     icon: image
-//   }, "<h1>Marker 0</h1><p>This is the home marker.</p>");
-
-// });
